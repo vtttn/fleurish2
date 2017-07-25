462e7e12fe06315cb5aa92ae7e5bebf600917f81
Kerry Russell <kdrussel@loyno.edu>
2017-07-25 10:35:16 -0500

    dropdowns

--- public/js/controllers/BrowseCtrl.js
+++ public/js/controllers/BrowseCtrl.js
@@ -7,7 +7,9 @@ angular.module('BrowseCtrl', []).controller('BrowseController', function($scope,
 		$scope.events = response.data;
 	});
 
+	$scope.types = ["art" , "cause" , "crafts" , "dance" , "drink" , "film" , "fitness" , "food" , "game" , "health" , "home" , "literature" , "music" , "networking" , "other" , "party" , "recreation" , "religion" , "shopping" , "theater" , "wellness"];
 	$scope.locations = ["New Orleans" , "Metairie" , "Gretna" , "Kenner" , "Avondale" , "Belle Chasse" , "Chalmette" , "Elmwood" , "Harahan" , "Harvey" , "Marrero"];
+	$scope.admissions = ["Free" , "Under $10" , "Under $25" , "Under $50" , "$75 and over"];
 	$scope.citySelect = null;
 
 		// {id: 1, value: "Metairie"},
@@ -21,6 +23,8 @@ angular.module('BrowseCtrl', []).controller('BrowseController', function($scope,
 		// {id: 9, value: "Harvey"}, 
 		// {id:10, value: "Marrero"}
 
+	$scope.filterTypebtn = function(){};
 	$scope.filterCitybtn = function(){};
+	$scope.filterAdmissionbtn = function(){};
 
 });
