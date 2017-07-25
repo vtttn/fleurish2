5308bd5c18303ec0427c313282112cf6072d42e7
Vicky Tran <tranvickyt@gmail.com>
2017-07-25 10:40:40 -0500

    dropdowns

--- public/js/controllers/BrowseCtrl.js
+++ public/js/controllers/BrowseCtrl.js
@@ -3,24 +3,22 @@ var URL = 'https://fleurish.herokuapp.com';
 
 angular.module('BrowseCtrl', []).controller('BrowseController', function($scope, $http) {
 
+	// populates data from DB
 	$http.get(URL+"/all-event").then(function(response){
 		$scope.events = response.data;
 	});
 
+	// dropdown of cities to select/filter
 	$scope.locations = ["New Orleans" , "Metairie" , "Gretna" , "Kenner" , "Avondale" , "Belle Chasse" , "Chalmette" , "Elmwood" , "Harahan" , "Harvey" , "Marrero"];
 	$scope.citySelect = null;
+	// click function for city filter
+	$scope.filterCitybtn = function(){
+		$scope.selectedCity;
+		console.log(selectedCity);
 
-		// {id: 1, value: "Metairie"},
-		// {id: 2, value: "Gretna"}, 
-		// {id: 3, value: "Kenner"}, 
-		// {id: 4, value: "Avondale"}, 
-		// {id: 5, value: "Belle Chasse"}
-		// {id: 6, value: "Chalmette"},
-		// {id: 7, value: "Elmwood"}, 
-		// {id: 8, value: "Harahan"}, 
-		// {id: 9, value: "Harvey"}, 
-		// {id:10, value: "Marrero"}
+	};
 
-	$scope.filterCitybtn = function(){};
+
+
 
 });
