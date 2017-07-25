ddf99f7c88d028e0e29ff879b2de4acbbc2306d2
Vicky Tran <tranvickyt@gmail.com>
2017-07-25 11:10:10 -0500

    console.log selects on all btns

--- public/js/controllers/BrowseCtrl.js
+++ public/js/controllers/BrowseCtrl.js
@@ -9,24 +9,26 @@ angular.module('BrowseCtrl', []).controller('BrowseController', function($scope,
 	});
 
 	// dropdown of event type
-	$scope.types = ["art" , "cause" , "crafts" , "dance" , "drink" , "film" , "fitness" , "food" , "game" , "health" , "home" , "literature" , "music" , "networking" , "other" , "party" , "recreation" , "religion" , "shopping" , "theater" , "wellness"];
+	$scope.types = ["Art" , "Cause" , "Crafts" , "Dance" , "Drink" , "Film" , "Fitness" , "Food" , "Game" , "Health" , "Home" , "Literature" , "Music" , "Networking" , "Other" , "Party" , "Recreation" , "Religion" , "Shopping" , "Theater" , "Wellness"];
+
 
 	// dropdown of cities to select/filter
 	$scope.locations = ["New Orleans" , "Metairie" , "Gretna" , "Kenner" , "Avondale" , "Belle Chasse" , "Chalmette" , "Elmwood" , "Harahan" , "Harvey" , "Marrero"];
-	// $scope.selectedCity = null;
 
 	// dropdown of admission price
 	$scope.admissions = ["Free!" , "Under $10" , "Under $25" , "Under $50" , "$75 and over"];
-	
+
 	// click function for city filter
 	$scope.filterCitybtn = function(){
-		// $scope.selectedCity;
 		console.log($scope.selectedCity);
 
 	};
 
-	$scope.filterTypebtn = function(){};
-	$scope.filterCitybtn = function(){};
-	$scope.filterAdmissionbtn = function(){};
+	$scope.filterTypebtn = function(){
+		console.log($scope.selectedType);
+	};
+	$scope.filterAdmissionbtn = function(){
+		console.log($scope.selectedAdmission);
+	};
 
 });
